dfbc9f6f101ec2c2bb0d4f306a72ffae10bb54a3
Fran Baez <103434629+franbaez16@users.noreply.github.com>
2022-05-16 12:08:48 -0300

    Add files via upload

---
+++ horas_espejos.js
@@ -0,0 +1,102 @@
+var respuesta;
+
+alert ("Horas espejo")
+alert ("00:00; 01:01; 02:02; 03:03; 04:04; 05:05; 06:06; 07:07; 08:08; 09:09; 10:10; 11:11; 12:12; 13:13; 14:14; 15:15; 16:16; 17:17; 18:18; 19:19; 20:20; 21:21; 22:22; 23:23")
+respuesta=prompt("¿De que hora quiere saber el significado?")
+
+if (respuesta == "00:00")
+{
+    alert("Una persona especial esta pensando en ti")
+}
+if (respuesta == "01:01")
+{
+    alert("alguien esta enamorad@ de ti")
+}
+if (respuesta == "02:02")
+{
+    alert("Te ocultan algo")
+}
+if (respuesta == "03:03")
+{
+    alert("Tienes desconfianza de alguien")
+}
+if (respuesta == "04:04")
+{
+    alert("Tendras un cambio")
+}
+if (respuesta == "05:05")
+{
+    alert("Posible futuro amor")
+}
+if (respuesta == "06:06")
+{
+    alert("Sos lo mas importante para alguien")
+}
+if (respuesta == "07:07")
+{
+    alert("Tus angeles te confirman positivamente eso que tanto dudas")
+}
+if (respuesta == "08:08")
+{
+    alert("Grandes cambios se acercan")
+}
+if (respuesta == "09:09")
+{
+    alert("Explota tu potencial interior")
+}
+if (respuesta == "10:10")
+{
+    alert("Fin de ciclos")
+}
+if (respuesta == "11:11")
+{
+    alert("Todo va a mejorar")
+}
+if (respuesta == "12:12")
+{
+    alert("Pide un deseo")
+}
+if (respuesta == "13:13")
+{
+    alert("Haz cambios en tu vida")
+}
+if (respuesta == "14:14")
+{
+    alert("Alejate de lo que te hace daño")
+}
+if (respuesta == "15:15")
+{
+    alert("Te estan recordando")
+}
+if (respuesta == "16:16")
+{
+    alert("Saca lo malo dentro de ti")
+}
+if (respuesta == "17:17")
+{
+    alert("Crea un nuevo tu")
+}
+if (respuesta == "18:18")
+{
+    alert("Sigue tu intuicion")
+}
+if (respuesta == "19:19")
+{
+    alert("Tienes que ser más paciente")
+}
+if (respuesta == "20:20")
+{
+    alert("La persona que amas piensa en ti")
+}
+if (respuesta == "21:21")
+{
+    alert("Sucederan cosas buenas")
+}
+if (respuesta == "22:22")
+{
+    alert("Lo que haz hecho tendra frutos")
+}
+if (respuesta == "23:23")
+{
+    alert("Cree y escucha")
+}
